0fd73f40b0d3711cfefb31001c59e33e0f51bbae
RajeshKumar B <rbongurala@innominds.com>
2018-05-17 14:14:20 +0530

    Configuration is added

---
+++ src/main/resources/ticketconfiguration.sql
@@ -0,0 +1,6 @@
+INSERT INTO ticket_configuration(id, property, value) VALUES (1, 'apischedulerflag', 'N');
+INSERT INTO ticket_configuration(id, property, value) VALUES (2, 'ftpschedulerflag', 'Y');
+INSERT INTO ticket_configuration(id, property, value) VALUES (3, 'apischedulertime', '*/240 * * * * *');
+INSERT INTO ticket_configuration(id, property, value) VALUES (4, 'ftpschedulertime', '*/240 * * * * *');
+INSERT INTO ticket_configuration(id, property, value) VALUES (5, 'apilastrundate', '2018-05-10 01:23:59');
+
