0f3f5649330c8b72cfe32c63a7b24f54d3b719d3
Karthik <kdhandu1@gmail.com>
2018-06-15 12:45:55 +0530

    Added a method ticket system repository

--- src/main/java/com/ims/repository/TicketSystemRepository.java
+++ src/main/java/com/ims/repository/TicketSystemRepository.java
@@ -10,4 +10,5 @@ public interface TicketSystemRepository extends JpaRepository<TicketSystem, Long
 	TicketSystem findBySystemNameAndCustomer(String systemName, String customer);
 	TicketSystem findBySystemNameAndCustomerAndEnableFlag(String systemName, String customer,String enableFlag);
 	List<TicketSystem> findByCustomerAndEnableFlagAndType(String customer,String enableFlag, String type);
+	List<TicketSystem> findByCustomer(String customer);
 }
