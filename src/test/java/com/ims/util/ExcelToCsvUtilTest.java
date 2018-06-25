295e0da81cf60cb00cf84caad55054013f680953
Karthik <kdhandu1@gmail.com>
2018-06-25 17:32:33 +0530

    Added test cases for ExcelToCsv

---
+++ src/test/java/com/ims/util/ExcelToCsvUtilTest.java
@@ -0,0 +1,23 @@
+package com.ims.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ExcelToCsvUtilTest {
+
+	@InjectMocks
+	ExcelToCsvUtil excelToCsvUtil;
+	
+	@Test
+	public void readExcelFile() {
+		excelToCsvUtil.readExcelFile("src/test/resources/Deloitte_AMPM_P10_20180522121000.xls", "src/test/resources/", "src/test/resources/");
+	}
+	
+	@Test
+	public void getRecordsCount() {
+		excelToCsvUtil.getRecordsCount("src/test/resources/Deloitte_AMPM_P10_20180522121000.xls");
+	}
+}
